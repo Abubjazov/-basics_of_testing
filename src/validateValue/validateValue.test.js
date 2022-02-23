5cde3743644af533b75901fc6a3ee139e3c44e37
abubjazov <abubjazov@mail.ru>
2022-02-23 19:25:31 +0300

    validateValue/: validateValue.test++

--- src/validateValue/validateValue.test.js
+++ src/validateValue/validateValue.test.js
@@ -1,6 +1,23 @@
 const validateValue = require('./validateValue')
 
+describe('Func: validateValue', () => {
+	test('should validate correct value', () => {
+		expect(validateValue(50)).toBe(true)
+	})
 
-test('should validate', () => {
-	expect(validateValue(50)).toBe(true)
+	test('should validate lower limit value', () => {
+		expect(validateValue(0)).toBe(true)
+	})
+
+	test('should validate upper limit value', () => {
+		expect(validateValue(100)).toBe(true)
+	})
+
+	test('should validate lower uncorrect value', () => {
+		expect(validateValue(-1)).toBe(false)
+	})
+
+	test('should validate upper uncorrect value', () => {
+		expect(validateValue(101)).toBe(false)
+	})
 })
